8728920816c8cf38920dba9e4432b77524d36f8f
Gabriele Bellomia <56808633+Bellomia@users.noreply.github.com>
2020-09-09 05:22:04 +0200

    Update semiHilbert.m

--- semiHilbert.m
+++ semiHilbert.m
@@ -1,6 +1,6 @@
 function H = semiHilbert(zeta,D)
 %% HILBERT Transform, assuming a semicircular density of states of radius D
-%  TO BE UNDERSTOOD AND COMMENTED BETTER...
+%  TO BE COMMENTED BETTER...
 s = sqrt(zeta.^2 - D^2);
 s = sign(imag(s)) .* s;
 H = 2 * (zeta - s) / D^2;
