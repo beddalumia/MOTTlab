8aa41048983b39617297559a250f53fb7108ebc0
Gabriele Bellomia <gbellomia@live.it>
2022-01-18 07:34:12 +0100

    Optimizing +plot.{U,T}line() functions
    Scattering point by point is dumb, full stop.

--- code/+plot/Tline.m
+++ code/+plot/Tline.m
@@ -1,11 +1,7 @@
 function line = Tline(Z,U,Tmin,Tstep,Tmax)
     line = figure("Name",'T-driven MIT');
-    i=0; T = Tmin;
-    while T <= Tmax
-        i = i + 1;
-        scatter(T,Z(i),'k','filled'); hold on
-        T = T + Tstep;
-    end
+    T = Tmin:Tstep:Tmax;
+    scatter(T,Z,'k','filled');
     title(sprintf('IPT  |  Quasiparticle weight at U/t = %f',2*U)) % (Units: D=2t)
     xlabel('$T$','Interpreter','latex')
     ylabel('$Z = M/M^*$','Interpreter','latex')
--- code/+plot/Uline.m
+++ code/+plot/Uline.m
@@ -1,12 +1,8 @@
 function line = Uline(Z,beta,Umin,Ustep,Umax)
     line = figure("Name",'U-driven MIT');
-    i=0; U = Umin;
-    while U <= Umax 
-        i = i + 1;
-        scatter(2*U,Z(i),'k','filled'); hold on % (Units: D=2t)
-        U = U + Ustep;
-    end
-    title(sprintf('IPT  |  Quasiparticle weight at T = %f',1/beta))
+    U = Umin:Ustep:Umax;
+    scatter(2.*U,Z,'k','filled'); hold on
+    title(sprintf('IPT  |  U-driven MIT at T = %f',1/beta))
     xlabel('$U/t$','Interpreter','latex')
     ylabel('$Z = M/M^*$','Interpreter','latex')
 end
