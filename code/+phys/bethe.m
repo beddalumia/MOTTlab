12f1a1bb5d42bc4725cef524d26764cfe6d464e5
Gabriele Bellomia <gbellomia@live.it>
2022-03-19 02:28:58 +0100

    Better doc/header for phys.bethe
    Following the lattices branch at 73eb54c :)

--- code/+phys/bethe.m
+++ code/+phys/bethe.m
@@ -1,26 +1,47 @@
 function gloc = bethe(zeta,D)
-%% SELF-CONSISTENCY relation for the Bethe lattice
-%  
-%  Most generally the DMFT self-consistency relation expresses the local
-%  GF as a the Fourier transform of the lattice GF evaluated at r=0, namely
-%  \sum_k G_latt and the self-energy appearing in this expression should be
+%% Local GF for the Bethe lattice with D half-bandwidth, evaluated at z ∈ ℂ
+%
+%  Input:
+%       zeta    : complex valued array, domain on which to evaluate the GF
+%       D       : real double, hald-bandwidth of the noninteracting system
+%  Output:
+%       gloc    : complex valued array, local GF, G(z) = ∫dε DOS(ε)/(z-ε)
+%
+%  You may recognize the integral expression of G(z) as the Hilbert transform
+%  of the noninteracting spectral function A₀(ω), aka density of states DOS,
+%  evaluated at complex argument z.
+%  But for proper use within a DMFT code it is *crucial* that z could really
+%  take *any* complex value, not just those on real- or imaginary-axis. This
+%  makes impossible to make use of the efficient hilbert() function provided
+%  within the Signal Processing Toolbox, as for the Kramers Kronig transform
+%  implemented in math.fkkt(). The reason for this very general requirement
+%  lies in the main application within DMFT: the self-consistency relation.
+%
+%  Most generally the DMFT self-consistency relation expresses the local GF
+%  as the lattice GF evaluated at r = 0, namely ∑ₖG(k,ω) = ∑ₖ1/(ω-εₖ-∑(k,ω))
+%  where the self-energy appearing should be assumed k-independent, hence 
 %  substituted with the impurity one (locality assumption), thus giving:
 %
-%          Gloc(z) = F_lattice[Sloc(z)],             DOS <--> lattice
+%      ∑ₖG(k,ω) = ∑ₖ1/(ω-εₖ-∑(ω)) = ∫dε DOS(ε)/(ω-ε-∑(ω)) = G(ω-∑(ω))
 %
-%                  = H[DOS](z-Sloc(z)),   i.e. F_lattice <--> H[DOS]
+%               = H[DOS](ω-∑(ω)),
 %
-%  where H[u](:) is the HILBERT transform of the function u(..)
+%  where H[u](z) is the HILBERT transform of the function u(:), evaluated
+%  at the most generic complex argument z = ω-∑(ω).
 %
-%  > On the BETHE lattice we can avoid computing numerically with hilbert()
-%    for that the DOS is very simple (semi-circular of radius D) and the 
-%    transform can be written explicitly as:
+%  To secure performance we *must* avoid computing such hilbert transform
+%  by direct integration, either by sum/trapz or adaptive quadrature, thus
+%  exploiting instead lattice-specific analytical insight.
 %
-%            H(z) = 2(z-sign[Re(z)]sqrt[z^2-D^2])/D^2
+%  > On the BETHE lattice we can virtually avoid any numerical computation
+%    for that the DOS is very simple (semicircle of radius D) and the
+%    transform can be integrated explicitly as:
 %
-%                 = 2(z-sign[Im(s)]*s)/D^2, with s = sqrt[z^2-D^2]
+%           G(z) = 2(z-sign[Re(z)]sqrt[z^2-D^2])/D^2
 %
-%    where the last passage is a trick to allow U=0 (Sloc(:)=0) evaluations 
+%                = 2(z-sign[Im(s)]*s)/D^2, with s = sqrt[z^2-D^2]
+%
+%    where the last passage is a trick to allow U=0 (∑(:)=0) evaluations. 
 %
 %% Theoretical Background at:
 %
@@ -38,4 +59,6 @@ function gloc = bethe(zeta,D)
     gloc = 2 * (zeta - p) / D^2;
     
 end
+
+
 
