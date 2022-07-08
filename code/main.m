54af7728fb7dac6cc926e54f9fb098f500daf3fa
Gabriele Bellomia <gbellomia@live.it>
2022-07-08 04:14:33 +0200

    Intermediate testing
    Using very very low mixing parameter, and letting many many loops (5000)
    we somewhat tame (but not solve) the U > 3.3 catastrophe: the points do
    not converge yet, but most of the time the final loop error is under 10%
    whereas at commit c46b705ecd2a1aef5954afcaa35b78d01a05063d we had crazy
    values, even over 100% (signaling breaking of gloc normalization...).
    
    This may call for some more complex mixing accelerator for convergence,
    either of the Anderson (adaptive) or Broyden type.
    
    See here for an overview: https://doi.org/10.1103/PhysRevB.38.12807

--- code/main.m
+++ code/main.m
@@ -19,7 +19,7 @@ latt = 'bethe';         % Noninteracting band-dispersion
 
 %% INPUT: Boolean Flags
 MottBIAS     = 0;       % Changes initial guess of gloc (strongly favours Mott phase)
-ULINE        = 0;       % Takes and fixes the given beta value and performs a U-driven line
+ULINE        = 1;       % Takes and fixes the given beta value and performs a U-driven line
 TLINE        = 0;       % Takes and fixes the given U value and performs a T-driven line
 UTSCAN       = 0;       % Ignores both given U and beta values and builds a full phase diagram
 SPECTRAL     = 1;       % Controls plotting of spectral functions
@@ -33,7 +33,7 @@ DEBUG        = 0;       % Activates debug prints / plots / operations
 FAST         = 1;       % Activates fast FFTW-based convolutions
 
 %% INPUT: Control Parameters
-mloop = 3000;           % Max number of DMFT iterations 
+mloop = 5000;           % Max number of DMFT iterations 
 err   = 1e-5;           % Convergence threshold for self-consistency
 mix   = 0.01;           % Mixing parameter for DMFT iterations (=1 means full update)
 wres  = 2^15;           % Energy resolution in real-frequency axis
